292f87bd0472bea8899332dc0892b2c9ce7f8085
WalterDane <walterdane1@icloud.com>
2020-09-29 00:18:59 -0500

    addition of route decorator

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from webob import Request, Response
 
 class API():
+    def __init__(self):
+        self.routes = {} #store the paths as keys and handlers as values
+
     def __call__(self, environ, start_response):
         """
         Each time a client makes an HTTP request, this function
@@ -15,13 +18,27 @@ class API():
 
         return response(environ, start_response)
 
+    def route(self, path):
+        """
+        This method is a decorator that accepts a path and wraps the handlers
+        --------------------------------------------------------------------
+        path: the path to the specified directory
+        """
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        
+        return wrapper
+
     def request_handler(self, request):
         """
+        This method will handle the request from the client
+        ---------------------------------------------------
         request: the request from the client
         """
         user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found") #identifies the application, OS, vender, and/or version of the requesting user agent
 
         response = Response()
-        response.text = f"Hello, {user_agent}"
+        response.text = f"Response from server, {user_agent}"
 
         return response
