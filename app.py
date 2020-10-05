4f7f515fcb3a7166cff620bc73f75ad85a8ea40c
WalterDane <walterdane1@icloud.com>
2020-10-05 02:45:08 -0500

    addition of put and delete method

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from api import API
 
 app = API() #calling instance of API class is the responsibility of the web server
 
+@app.route("/")
+def default(request, response):
+    response.text = "Hello from local host"
+
 @app.route("/home")
 def home(request, response):
     response.text = "Hello from the HOME page"
@@ -9,3 +13,17 @@ def home(request, response):
 @app.route("/about")
 def about(request, response):
     response.text = "Hello from the ABOUT page"
+
+@app.route("/book")
+class BooksResource:
+    def get(self, request, response):
+        response.text = "Books Page"
+
+    def post(self, request, response):
+        response.text = "Endpoint to create a book"
+    
+    def put(self, request, response):
+        response.text = "Replacing current representation of the target resource"
+
+    def delete(self, request, response):
+        response.text = "Deleting the specified resoue"
