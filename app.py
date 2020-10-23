286133a0fc445d9f41347ad211f6e4f9516bb5b5
WalterDane <walterdane1@icloud.com>
2020-10-23 05:23:42 -0500

    refactoring

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from api import API
 
+"""
+Exceptions
+"""
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
 app = API() #calling instance of API class is the responsibility of the web server
 
+app.add_exception_handler(custom_exception_handler)
+
+"""
+Directories
+"""
 @app.route("/")
 def default(request, response):
     response.text = "Hello from local host"
@@ -14,9 +25,16 @@ def home(request, response):
 def about(request, response):
     response.text = "Hello from the ABOUT page"
 
+"""
+Templating
+"""
 @app.route("/template")
 def template_handler(request, response):
     response.body = app.get_template(
         "index.html",
         context={"name": "Ackee", "title": "Legit framework"}
     ).encode()
+
+@app.route("/exception")
+def throw_handler_exception(request, response):
+    raise AssertionError("This handler should not be used")
