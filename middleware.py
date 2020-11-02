ae7f3a265cba70277ab14a3b70de9e44c89673e3
WalterDane <walterdane1@icloud.com>
2020-11-01 23:03:53 -0600

    processing now prints times

--- middleware.py
+++ middleware.py
@@ -1,19 +1,28 @@
+import time
+from webob import Request
+
 class Middleware:
     def __init__(self, app):
         self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.app.handle_request(request)
+
+        return response(environ, start_response)
 
     def add(self, middleware_cls):
         self.app = middleware_cls(self.app)
 
     def process_request(self, request):
-        pass
+        print("processing request at time: " + str(time.time()))
 
     def process_response(self, request, response):
-        pass
+        print("processing response at time: " + str(time.time()))
 
     def handle_request(self, request):
         self.process_request(request)
-        response = self.app.handle_client_request(request)
+        response = self.app.handle_request(request)
         self.process_response(request, response)
 
         return response
